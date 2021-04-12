7a74918ecc5d24ddd8aeba981302ed60dcfbe93f
Geraldo Wehmhoff <wehmhoff@gmail.com>
2021-04-12 19:33:02 -0300

    Correção de erro de build

--- src/utilities/MessagingUtils.ts
+++ src/utilities/MessagingUtils.ts
@@ -133,8 +133,8 @@ export class MessagingUtils {
          description =
             //`React with ${Base.getConfig().joinEmoji} or type \`${queueGuild.prefix}${Base.getCmdConfig().joinCmd} ` +
             //`${queueChannel.name}\` to join or leave this queue.`;
-            `Escreve "\`${queueGuild.prefix}${Base.getCmdConfig().joinCmd} ` +
-            `${queueChannel.name}\` dino" para entrares ou saíres desta lista. É obrigatório colocar os dinos em frente do nome.`;
+            `Escreve "\`${queueGuild.prefix}${Base.getCmdConfig().joinCmd} ${queueChannel.name}\` dino" para entrares ou saíres desta lista. 
+            É obrigatório colocar os dinos em frente do nome.`;
       }
       if (storedQueueChannel.header) {
          description += `\n\n${storedQueueChannel.header}`;
