36864dd9c69c8707c369ac07d7c3140095579960
Geraldo Wehmhoff <geraldo.wehmhoff@cmc.pr.gov.br>
2023-02-28 09:20:30 -0300

    Mudança na string de conexão do BD

--- src/utilities/Base.ts
+++ src/utilities/Base.ts
@@ -4,52 +4,64 @@ import { knex, Knex } from "knex";
 import { CommandConfigJson, ConfigJson } from "./Interfaces";
 
 export class Base {
-   public static getConfig(): ConfigJson {
-      return this.config;
-   }
+  public static getConfig(): ConfigJson {
+    return this.config;
+  }
 
-   public static getCmdConfig(): CommandConfigJson {
-      return this.cmdConfig;
-   }
+  public static getCmdConfig(): CommandConfigJson {
+    return this.cmdConfig;
+  }
 
-   public static getKnex(): Knex {
-      return this.knex;
-   }
+  public static getKnex(): Knex {
+    return this.knex;
+  }
 
-   public static getClient(): Client {
-      return this.client;
-   }
+  public static getClient(): Client {
+    return this.client;
+  }
 
-   public static isMe(member: GuildMember): boolean {
-      return member.id === member.guild.me.id;
-   }
+  public static isMe(member: GuildMember): boolean {
+    return member.id === member.guild.me.id;
+  }
 
-   protected static config: ConfigJson = JSON.parse(readFileSync("../config/config.json", "utf8"));
-   protected static cmdConfig: CommandConfigJson = JSON.parse(readFileSync("../config/command-config.json", "utf8"));
+  protected static config: ConfigJson = JSON.parse(
+    readFileSync("../config/config.json", "utf8")
+  );
+  protected static cmdConfig: CommandConfigJson = JSON.parse(
+    readFileSync("../config/command-config.json", "utf8")
+  );
 
-   protected static knex = knex({
-      client: Base.config.databaseType,
-      connection: {
-         database: Base.config.databaseName,
-         host: Base.config.databaseHost,
-         password: Base.config.databasePassword,
-         user: Base.config.databaseUsername,
+  protected static knex = knex({
+    client: Base.config.databaseType,
+    // connection: {
+    //    database: Base.config.databaseName,
+    //    host: Base.config.databaseHost,
+    //    password: Base.config.databasePassword,
+    //    user: Base.config.databaseUsername,
+    // },
+    connection: process.env.DATABASE_URL,
+  });
+
+  protected static client = new Client({
+    messageCacheLifetime: 12 * 60 * 60, // Cache messages for 12 hours
+    messageCacheMaxSize: 2, // Cache up to 2 messages per channel
+    messageEditHistoryMaxSize: 0, // Do not cache edits
+    messageSweepInterval: 1 * 60 * 60, // Sweep every hour
+    partials: ["MESSAGE", "REACTION", "USER"],
+    presence: {
+      activity: {
+        type: `LISTENING`,
+        name: `${Base.config.prefix}${Base.cmdConfig.helpCmd}`,
       },
-   });
-
-   protected static client = new Client({
-      messageCacheLifetime: 12 * 60 * 60, // Cache messages for 12 hours
-      messageCacheMaxSize: 2, // Cache up to 2 messages per channel
-      messageEditHistoryMaxSize: 0, // Do not cache edits
-      messageSweepInterval: 1 * 60 * 60, // Sweep every hour
-      partials: ["MESSAGE", "REACTION", "USER"],
-      presence: {
-         activity: {
-            type: `LISTENING`,
-            name: `${Base.config.prefix}${Base.cmdConfig.helpCmd}`,
-         },
-         status: "online",
-      },
-      ws: { intents: ["GUILDS", "GUILD_VOICE_STATES", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS"] },
-   });
+      status: "online",
+    },
+    ws: {
+      intents: [
+        "GUILDS",
+        "GUILD_VOICE_STATES",
+        "GUILD_MESSAGES",
+        "GUILD_MESSAGE_REACTIONS",
+      ],
+    },
+  });
 }
